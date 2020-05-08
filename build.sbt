cf6c4b0906fe405d37e12096440cc480f5e142a7
Kara Valentine <gitkraken@archmage.me>
2020-05-08 12:42:05 +1000

    It works now!

--- build.sbt
+++ build.sbt
@@ -2,7 +2,9 @@ name := "primrose"
 
 version := "0.1"
 
-scalaVersion := "2.13.2"
+scalaVersion := "2.12.0"
 
 resolvers += Resolver.JCenterRepository
 libraryDependencies += "net.katsstuff" %% "ackcord" % "0.16.1"
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.3"
+libraryDependencies += "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,28 @@
+import ackcord.requests.{CreateMessage, CreateMessageData}
+import ackcord.{APIMessage, ClientSettings}
+import cats.implicits._
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import scala.io.Source
+
 object Main extends App {
 
+  val tokenFilename = "token.txt"
+  val stream = Source.fromFile(tokenFilename)
+  val token = stream.getLines().collectFirst { case x => x }
+  stream.close()
+
+  val clientSettings = ClientSettings(token.get)
+  import clientSettings.executionContext
+  val client = Await.result(clientSettings.createClient(), Duration.Inf)
+
+  client.onEventAsync { implicit c => {
+    case APIMessage.MessageCreate(message, _) =>
+      val response = CreateMessage(message.channelId, CreateMessageData(content = "Hi there!"))
+      client.requestsHelper.run(response).map(_ => ())
+    }
+  }
+
+  client.login()
 }
