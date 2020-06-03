c3886dd3bf4888c1e9d3305282e1abc3ec9ec195
Kara Valentine <gitkraken@archmage.me>
2020-06-04 03:33:34 +1000

    dev stuff

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "primrose"
 
 version := "0.1"
 
-scalaVersion := "2.12.0"
+scalaVersion := "2.13.2"
 
 resolvers += Resolver.JCenterRepository
 libraryDependencies += "net.katsstuff" %% "ackcord" % "0.16.1"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,9 @@
-import ackcord.data.GuildMessage
-import ackcord.requests.{CreateMessage, CreateMessageData}
-import ackcord.{APIMessage, ClientSettings, EventRegistration}
+import java.time.{LocalDateTime, ZoneId, ZonedDateTime}
+
+import ackcord.cachehandlers.CacheSnapshotBuilder
+import ackcord.data.{ChannelId, GuildMessage, RoleId, TextChannelId}
+import ackcord.requests.{AddGuildMemberRole, CreateMessage, CreateMessageData, GetChannel}
+import ackcord.{APIMessage, CacheSnapshot, CacheState, ClientSettings, EventRegistration}
 import cats.implicits._
 
 import scala.concurrent.Await
@@ -16,21 +19,50 @@ object Main extends App {
 
   val clientSettings = ClientSettings(token.get)
   import clientSettings.executionContext
-  val client = Await.result(clientSettings.createClient(), Duration.Inf)
+  val client = Await.result(clientSettings.createClient(), Duration(15, "second"))
 
   client.onEventAsync { implicit c => {
-    case APIMessage.MessageCreate(message, _) if message.content.toLowerCase.contains("primrose") =>
+    case APIMessage.MessageCreate(message, _) if message.isInstanceOf[GuildMessage] &&
+     message.content.toLowerCase.contains("primrose") =>
 
-      // This is really bad!!!
       val guildMessage = message.asInstanceOf[GuildMessage]
 
-      
+      val name = guildMessage.member.nick.getOrElse(message.authorUser.get.username)
 
-      val messageData = CreateMessageData(content = s"Hi, ${guildMessage.member.nick.get.split(" ").head}!")
+      val messageData = CreateMessageData(content = s"Hi, ${name.split(" ").head}!")
       val response = CreateMessage(message.channelId, messageData)
       client.requestsHelper.run(response).map(_ => ())
     }
   }
 
+  client.onEventAsync { implicit c => {
+    case APIMessage.MessageCreate(message, _) if message.isInstanceOf[GuildMessage] &&
+      message.channelId == TextChannelId(707560505903939675L) =>
+
+      val guildMessage = message.asInstanceOf[GuildMessage]
+
+      val roleId = RoleId(717506376426979448L)
+      val response = AddGuildMemberRole(guildMessage.guildId, message.authorUserId.get, roleId)
+
+      client.requestsHelper.run(response).map(_ => ())
+  }}
+
   client.login()
+
+  // -- dumb stuff below --
+
+
+  val messageData = CreateMessageData(content = s"_squeak_")
+  val timedResponse = CreateMessage(TextChannelId(707560505903939675L), messageData)
+
+  new Thread {
+    override def run: Unit = {
+      while(true) {
+        Thread.sleep(10000)
+        client.requests.single(timedResponse).map(_ => ())
+        println(LocalDateTime.now().atZone(ZoneId.of("Australia/Melbourne")))
+      }
+    }
+  }.run
+
 }
